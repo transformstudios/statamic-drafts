4dbaa70f89a5fe707ccc36ab434ade2836394b16
edalzell <erin@thedalzells.org>
2020-01-21 09:59:31 -0800

    change JS loading (part of #2)

--- Drafts/resources/assets/js/cp.js
+++ Drafts/resources/assets/js/cp.js
@@ -1,4 +1,12 @@
-window.addEventListener("DOMContentLoaded", function () {
+if (document.readyState !== 'loading') {
+    addDrafts();
+} else {
+    document.addEventListener('DOMContentLoaded', function () {
+        addDrafts();
+    });
+}
+
+function addDrafts() {
     let html = `
         <button aria-expanded="false" aria-haspopup="true" class="btn dropdown-toggle" data-toggle="dropdown" type="button">
             <span class="caret"></span> <span class="sr-only">Toggle Dropdown</span>
@@ -13,4 +21,4 @@ window.addEventListener("DOMContentLoaded", function () {
     buttonGroup
         .addClass('btn-group')
         .append(html);
-});
+}
